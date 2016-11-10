fa645a05567ef0f2cdc61546ae6bb286542fa729
ChulHyun, Baek <chbaek@outlook.com>
2016-11-10 16:20:52 +0900

    Create ssh_keygen.sh

---
+++ ssh_keygen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# -- chb@mz.co.kr , Nov 10, 2016
+
+# -- Create a public Key in Bastion EC2 Instance -- #
+ssh-keygen -t rsa -b 2048 -v
+cat ~/.ssh/id_rsa.pub
+
+# -- And Copy Above Strings -- #
+# Paste Public Key at authorized_keys file of ec2-user
+cat >> authorized_keys 
+
+# -- End of Line -- #
+
