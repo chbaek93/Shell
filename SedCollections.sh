690124cbcb1c5795c68b99c28d9d93e54f4258cd
ChulHyun, Baek <chbaek@outlook.com>
2016-11-10 14:18:56 +0900

    Create SedCollections.sh

---
+++ SedCollections.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# --  Nov 10, 2016
+
+# -- Execute 'ls -l ' -- # 
+sed 's/^/ls -l /e' data.txt
+
+# -- Change Multiple word -- #
+sed '{
+  s/This/That/
+  s/What/Where/
+}' data.txt
+
+# -- Enclose the number -- #
+sed 's/^[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9]/[&]/g' data.txt
+
+# -- Enclose the whole line -- #
+sed 's/^.*/[&]/' data.txt
+
+# -- Choice User Name -- #
+sed 's/\([^:]*\).*/\1/' /etc/passwd
+
+# -- Enclose first character -- #
+sed 's/\(\b[A-Z]\)/\(\1\)/g' data.txt
+
+
