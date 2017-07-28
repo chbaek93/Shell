a7ab25925a58927697ab14a6b6bea59615d52312
ChulHyun, Baek <chbaek@outlook.com>
2017-07-28 09:27:58 +0900

    Update SystemCTL.sh

--- SystemCTL.sh
+++ SystemCTL.sh
@@ -8,9 +8,30 @@
 # -- /run/systemd/system/ : Systemd unit files created at run time. This directory takes precedence over the directory with installed service unit files. -- # 
 # -- /etc/systemd/system/ : Systemd unit files created by systemctl enable as well as unit files added for extending a service. -- # 
 
-
+# -- 0 : Shut down and power off the system -- # 
+# -- 1 : Set up a rescue shell -- # 
+# -- 2 : Set up a non-graphical multi-user system. -- # 
+# -- 3 : Set up a non-graphical multi-user system. -- # 
+# -- 4 : Set up a non-graphical multi-user system. -- # 
+# -- 5 : Set up a graphical multi-user system. -- # 
+# -- 6 : Shut down and reboot the system -- # 
 
 # sctl=systemctl
+
+systemctl get-default 
+
+systemctl set-default name.target 
+systemctl isolate name.target 
+
+# -- EX 
+systemctl set-default multi-user.target 
+systemctl isolate multi-user.target 
+systemctl --no-wall rescue 
+
+systemctl rescue # -- Changing to Rescue Mode -- # 
+systemctl emergency 
+systemctl --no-wall emergency 
+
 
 systemctl list-units 
 systemctl list-units --type=service 
@@ -19,6 +40,7 @@ systemctl list-unit-files
 systemctl start name.service 
 systemctl stop name.service 
 systemctl restart name.service 
+systemctl disable name.service 
 
 # -- Restarts a service only if it is running -- # 
 systemctl try-restart name.service 
@@ -36,4 +58,52 @@ systemctl list-units --type service -all
 # -- 
 systemctl mask name.service 
 systemctl unmask name.service 
+
+# -- 
+systemctl enable name.service 
+systemctl disable name.service 
+systemctl status name.service 
+systemctl list-unit-files --type service 
+systemctl list-dependencies --after httpd.service 
+systemctl list-dependencies --before httpd.service 
+
+# -- Shutting Down the System -- # 
+
+systemctl poweroff 
+systemctl halt 
+systemctl --no-wall poweroff 
+shutdown --poweroff hh:mm 
+
+shutdown --halt +m 
+shutdown -c # -- Canceled by the root user -- # 
+systemctl reboot 
+systemctl suspend 
+systemctl hibernate
+systemctl hybrid-sleep 
+
+# -- Controlling systemd on a remote machine -- # 
+systemctl -H root@server-01.example.com status httpd.service 
+
+# -- Create unit file -- # 
+touch /etc/systemd/system/name.service 
+chmod 664 /etc/systemd/system/name.service 
+
+# -- Unit File : name.service -- # 
+[Unit]
+Description=service_description
+After=network.target
+
+[Service]
+ExecStart=path_to_executable
+Type=forking
+PIDFile=path_to_pidfile
+
+[Install]
+
+systemctl daemon-reload 
+systemctl start name.service 
+
+
+WantedBy=default.target
+
 
